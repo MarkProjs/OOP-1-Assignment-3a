d0b24b72f8100b0225da11e17e240533a54181ba
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2024-11-06 18:08:20 -0500

    adding the atm class

---
+++ Assignment 3a/src/Atm.java
@@ -0,0 +1,45 @@
+public class Atm {
+    private double totalAmount;
+
+    public Atm() {
+        this.totalAmount = 0;
+    }
+
+    public void checkBalance() {
+        System.out.println("Your balance is: "+ this.totalAmount);
+    }
+
+    public void deposit(int amountDeposit) {
+        this.totalAmount += amountDeposit;
+        System.out.println("Deposit successful!");
+    }
+
+    public String withdraw(int amountWithdraw) {
+        String bills = "You will receive: " + "\n";
+        if(amountWithdraw > this.totalAmount) {
+            bills = "You cannot withdraw more than the current balance.";
+        }
+        else if(amountWithdraw % 5 != 0) {
+            bills="Invalid withdrawal amount! You can only withdraw amounts that are multiples of 5, 10, 50, or 100";
+        } else {
+            this.totalAmount -= amountWithdraw;
+            while (amountWithdraw > 0 ) {
+                if(amountWithdraw / 100 != 0) {
+                    bills += "\t"+ (amountWithdraw /100)+" bill(s) of $100"+"\n";
+                    amountWithdraw %=100;
+                } else if(amountWithdraw / 50 != 0) {
+                    bills += "\t"+ (amountWithdraw /50)+" bill(s) of $50"+"\n";
+                    amountWithdraw %=50;
+                } else if (amountWithdraw / 10 != 0) {
+                    bills += "\t"+ (amountWithdraw /10)+" bill(s) of $10"+"\n";
+                    amountWithdraw %=10;
+                } else if (amountWithdraw / 5 != 0) {
+                    bills += "\t"+ (amountWithdraw /5)+" bill(s) of $5"+"\n";
+                    amountWithdraw %=5;
+                }
+            }     
+        }
+        return bills;
+
+    }
+}
--- Assignment 3a/src/Main.java
+++ Assignment 3a/src/Main.java
@@ -4,13 +4,49 @@
 //For COMP 248 S - Fall 2024
 //-------------------------
 
-//Java program that emulates an ATM. The progrma will allow the users to deposit money, withdraw and check their balance.
+//Java program that emulates an ATM. The program will allow the users to deposit money, withdraw and check their balance.
 //
 import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Scanner input = new Scanner(System.in);
+		Atm atm = new Atm();
+		System.out.println("Welcome to the Best Bank ATM");
+		boolean exitProgram = false;
+		do {
+			System.out.println("------------------------------");
+			System.out.println("Please choose an option: ");
+			System.out.println("\t"+ "1. Check Balance");
+			System.out.println("\t"+ "2. Deposit");
+			System.out.println("\t"+ "3. Withdraw");
+			System.out.println("\t"+ "1. Exit");
+			System.out.print("Enter your choice: ");
+			int userChoice = input.nextInt();
+			switch (userChoice) {
+				case 1:
+					atm.checkBalance();
+					break;
+				case 2:
+					System.out.print("Enter amount to deposit: ");
+					int deposit = input.nextInt();
+					atm.deposit(deposit);
+					break;
+				case 3:
+					System.out.print("Enter amount to withdraw(multiples of 5, 10, 50, or 100): ");
+					int withdraw = input.nextInt();
+					atm.withdraw(withdraw);
+					break;
+				case 4:
+					System.out.println("Goodbye!");
+					exitProgram = true;
+					break;
+				default:
+					System.out.println("Invalid choice! Please try again.");
+					break;
+			}
+			System.out.println();
+		} while (exitProgram);
 		
 	}
 
