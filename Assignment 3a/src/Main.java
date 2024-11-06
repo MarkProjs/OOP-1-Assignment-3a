4d4079a0ad8d36d5a620414d217075afd566e7d0
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2024-11-06 16:34:42 -0500

    adding the assignment 3a project

---
+++ Assignment 3a/src/Main.java
@@ -0,0 +1,17 @@
+//-------------------------
+//Assignment 3
+//Written by: Mark Benedict Agluba, 40294956
+//For COMP 248 S - Fall 2024
+//-------------------------
+
+//Java program that emulates an ATM. The progrma will allow the users to deposit money, withdraw and check their balance.
+//
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	}
+
+}
